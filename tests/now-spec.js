267e4fe4f4884abacf8f669b4c60fbfba03bca4c
afc163 <afc163@gmail.com>
2014-08-18 19:39:59 +0800

    update tests

--- tests/now-spec.js
+++ tests/now-spec.js
@@ -1,10 +1,15 @@
 var expect = require('expect');
 var now = require('../index');
+var moment = require('moment');
 
 describe('now', function() {
 
-  it('normal usage', function() {
+  it('should be a string', function() {
     expect(now).to.be.a('string');  // add this
+  });
+
+  it('should be a valid date string', function() {
+    expect(moment(now, 'MMMM Do YYYY, h:mm:ss a').isValid()).to.be(true);
   });
 
 });
