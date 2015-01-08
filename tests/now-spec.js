67285ac8ff740f558e9fbc8e017fc1ff555edaf6
afc163 <afc163@gmail.com>
2015-01-08 22:28:39 +0800

    fix tst

--- tests/now-spec.js
+++ tests/now-spec.js
@@ -6,11 +6,11 @@ var jquery = require('jquery');
 describe('now', function() {
 
   it('should be a string', function() {
-    expect(now).to.be.a('string');  // add this
+    expect(now()).to.be.a('string');  // add this
   });
 
   it('should be a valid date string', function() {
-    expect(moment(now, 'MMMM Do YYYY, h:mm:ss a').isValid()).to.be(true);
+    expect(moment(now(), 'MMMM Do YYYY, h:mm:ss a').isValid()).to.be(true);
   });
 
 });
